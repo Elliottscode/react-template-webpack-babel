08a5b873488cfd7d4021f7f994e55829541c0a5c
Elliottscode <elliottmccully@yahoo.com>
2021-11-24 20:07:41 +0000

    adding require scss
    fixeing not live update scss

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('file-loader?name=[name].[ext]!./index.html');
+// require('file-loader?name=[')
 import React from "react";
 import  ReactDOM from "react-dom";
 import { App } from "./app";
